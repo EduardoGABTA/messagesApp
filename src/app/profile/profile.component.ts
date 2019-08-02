e1281e029a40faa809060e49b065728012802665
Eduardo Garcia <egarciab@ids.com.mx>
2019-08-02 11:55:43 -0500

    Ajuste al perfil de usuario(Upload Image)

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,6 +4,7 @@ import {UserService} from '../services/user.service';
 import {AuthenticationService} from '../services/authentication.service';
 import {ImageCroppedEvent} from 'ngx-image-cropper';
 import { AngularFireStorage } from 'angularfire2/storage'
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-profile',
@@ -39,12 +40,13 @@ export class ProfileComponent implements OnInit {
       pictures.then((resultado) => {
         this.picture = this.angularFireStorage.ref('pictures/' + currentPictureId + '.jpg').getDownloadURL();
         this.picture.subscribe((url) => {
-          this.userService.setAvatar(url, this.user.uid).then(() => [
-            alert('Avatar Subido Correctamente')
-          ]).catch((error) => {
-            alert('Ocurrio un error al tratar de subir la imagen');
+          this.userService.setAvatar(url, this.user.uid).then(() => {
+            Swal.fire('Perfil', 'Foto de perfil actualizada correctamente', 'success');
+          }).catch((error) => {
+            Swal.fire('Perfil', 'Ocurrio un error al tratar de subir la imagen', 'error');
             console.log(error);
           });
+          this.editandoFotoPerfil = !this.editandoFotoPerfil;
         });
       }).catch((error) => {
         console.log(error);
@@ -52,15 +54,18 @@ export class ProfileComponent implements OnInit {
     } else {
       this.userService.modifyUser(this.user).then((data) => {
         console.log(data);
-        alert('Datos guardados');
+        Swal.fire('Perfil', 'Datos actualizados correctamente', 'success');
       }, (error) => {
+        Swal.fire('Perfil', 'No se pudo actualizar los datos', 'success');
         console.log(error);
       });
+      this.editandoFotoPerfil = !this.editandoFotoPerfil;
     }
   }
 
   fileChangeEvent(event: any): void {
     this.imageChangedEvent = event;
+    this.editandoFotoPerfil = !this.editandoFotoPerfil;
   }
   imageCropped(event: ImageCroppedEvent) {
     this.croppedImage = event.base64;
